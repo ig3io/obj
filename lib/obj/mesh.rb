4bcba66ce4c5424d6fbbd467f6befe313817ca98
Ignacio Contreras Pinilla <ignacio.con.pin@gmail.com>
2014-05-22 11:09:14 +0200

    Implement basic parsing of actual OBJ data
    - Before, the vertices, normals, and faces arrays contained dummy data
      (although its sizes were correct)
    
    - Now they contain (correct?) data (tests pending)

--- lib/obj/mesh.rb
+++ lib/obj/mesh.rb
@@ -1,5 +1,9 @@
 module OBJ
   class Mesh
+    def self.load(file_path)
+      Mesh.new(file_path)
+    end
+
     attr_accessor :vertices, :normals, :text_coords, :faces
 
     def initialize(file_path)
@@ -8,22 +12,7 @@ module OBJ
       @text_coords = []
       @faces = []
 
-      File.open(file_path, 'r') do |f|
-        f.each_line do |line|
-          case line.split.first
-          when 'v'
-            @vertices << 0.0
-          when 'vt'
-            @text_coords << 0.0
-          when 'vn'
-            @normals << 0.0
-          when 'f'
-            @faces << 0.0
-          when 'mtllib'
-            # fail ArgumentError, 'invalid file (mtllib not supported)'
-          end
-        end
-      end
+      initialize_from_file(file_path)
     end
 
     def normals?
@@ -37,6 +26,28 @@ module OBJ
     def to_s
       "<#{self.class} vertices: #{@vertices.size} normals: #{@normals.size}" \
       " text_coords: #{@text_coords.size} faces: #{@faces.size}>"
+    end
+
+    private
+
+    def initialize_from_file file_path
+      File.open(file_path, 'r') do |f|
+        f.each_line do |line|
+          type, *data = line.split
+          case type
+          when 'v'
+            @vertices << data.map {|d| d.to_f}
+          when 'vt'
+            @text_coords << data.map {|d| d.to_f}
+          when 'vn'
+            @normals << data.map {|d| d.to_f}
+          when 'f'
+            @faces << data.map {|v| v.split('/').map {|attr| attr.to_i}}
+          when 'mtllib'
+            # TODO: not implemented
+          end
+        end
+      end
     end
   end
 end
